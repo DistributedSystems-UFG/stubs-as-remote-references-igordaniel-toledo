3c391a7d9c271852251e707b3f8fd1f4912c9baf
Igor Toledo <105389445+igordaniel-toledo@users.noreply.github.com>
2026-05-05 22:49:33 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,39 +1,38 @@
-import pickle, os, multiprocessing        #-
-from socket   import *  #-
-from random   import *  #-
-from constRPC import *  #-
-#-
+import pickle
+from socket import *
+from constRPC import *
+
 class Server:
-  def __init__(self, port=PORTS): #-
-    self.host = 'localhost'               # this machine                     #-
-    self.port = port                      # the port it will listen to       #-
-    self.sock = socket()                  # socket for incoming calls        #-
-    self.sock.bind((self.host,self.port)) # bind socket to an address        #-
-    self.sock.listen(5)                   # max num of connections           #-
-    self.setOfLists = {}                  # init: no lists to manage         
+    def __init__(self, host, port=PORTS):
+        self.host = host
+        self.port = port
+        self.sock = socket()
+        self.sock.bind((self.host, self.port))
+        self.sock.listen(5)
+        self.setOfLists = {}
 
-  def run(self):
-    while True:
-      (conn, addr) = self.sock.accept() # accept incoming call
-      data = conn.recv(1024)            # fetch data from client 
-      request = pickle.loads(data)      # unwrap the request
+    def run(self):
+        while True:
+            (conn, addr) = self.sock.accept()
+            data = conn.recv(1024)
+            request = pickle.loads(data)
 
-      if request[0] == CREATE:               # create a list       
-        listID = len(self.setOfLists) + 1    # allocate listID
-        self.setOfLists[listID] = []         # initialize to empty
-        conn.send(pickle.dumps(listID))      # return ID
+            if request[0] == CREATE:
+                listID = len(self.setOfLists) + 1
+                self.setOfLists[listID] = []
+                conn.send(pickle.dumps(listID))
 
-      elif request[0] == APPEND:             # append request
-        listID = request[2]                  # fetch listID
-        data   = request[1]                  # fetch data to append
-        self.setOfLists[listID].append(data) # append it to the list
-        conn.send(pickle.dumps(OK))          # return an OK
+            elif request[0] == APPEND:
+                listID = request[2]
+                data   = request[1]
+                self.setOfLists[listID].append(data)
+                conn.send(pickle.dumps(OK))
 
-      elif request[0] == GETVALUE:           # read request
-        listID = request[1]                  # fetch listID
-        result = self.setOfLists[listID]     # get the elements
-        conn.send(pickle.dumps(result))      # return the list
-#-
-      elif request[0] == STOP:               # request to stop       #-
-        conn.close()                         # close the connection  #-
-        break                                                        #-
+            elif request[0] == GETVALUE:
+                listID = request[1]
+                result = self.setOfLists[listID]
+                conn.send(pickle.dumps(result))
+
+            elif request[0] == STOP:
+                conn.close()
+                break
